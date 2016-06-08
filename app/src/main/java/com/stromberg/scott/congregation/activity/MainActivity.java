39d080c40a477fa41bb21203cabe7b72545546b3
Scott Stromberg <srstromberg@gmail.com>
2016-06-07 19:54:33 -0600

    initial commit

---
+++ app/src/main/java/com/stromberg/scott/congregation/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.stromberg.scott.congregation.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.stromberg.scott.congregation.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
